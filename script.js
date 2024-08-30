5e6d6a0a7c648855d9a5bf9a6e5716b88391e855
anikaivnath <anikaivnath8@gmail.com>
2024-08-31 00:32:14 +0600

    Completed

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
-let nav=document.getElementById('nav');
-let icon=document.getElementById('icon');
-let  nav2=document.getElementById('nav2');
-let icon2=document.getElementById('icon2')
-nav.addEventListener('mouseover',()=>{
-    icon.style.display='block';
-})
-nav.addEventListener('mouseout',()=>{
-    icon.style.display='none';
-})
+let nav = document.getElementById('nav');
+let icon = document.getElementById('icon');
+let nav2 = document.getElementById('nav2');
+let icon2 = document.getElementById('icon2');
+let txt= document.querySelectorAll('.txt');
+
+
+nav.addEventListener('mouseover', () => {
+    icon.style.display = 'block';
+});
+
+nav.addEventListener('mouseout', () => {
+    icon.style.display = 'none';
+});
+
+icon.addEventListener('click', () => {
+    txt.forEach(section => {
+       
+        nav.style.transition = 'width 1s ease, transform 3s ease';        
+        nav.style.transform = 'translateX(-300px)'
+        nav2.style.display = 'block';
+        section.style.marginLeft = '100px';
+    
+    });
+   
+   
+   
+});
+
+
