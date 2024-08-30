af0df593b7a0f96db4ab4e616875d23d7db06f53
anikaivnath <anikaivnath8@gmail.com>
2024-08-30 19:28:55 +0600

    add icons

---
+++ script.js
@@ -0,0 +1,10 @@
+let nav=document.getElementById('nav');
+let icon=document.getElementById('icon');
+let  nav2=document.getElementById('nav2');
+let icon2=document.getElementById('icon2')
+nav.addEventListener('mouseover',()=>{
+    icon.style.display='block';
+})
+nav.addEventListener('mouseout',()=>{
+    icon.style.display='none';
+})
